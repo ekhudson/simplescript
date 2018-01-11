1548557d1159472b8088b6d9ef909dc60b20ba56
Elliot Hudson <ekhudson@gmail.com>
2018-01-10 19:34:04 -0800

    Added Noise Component
    Ticker Component got moved

---
+++ SimpleScript/SimpleScript/SimpleScript/Actions/MoveTransformAction.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SimpleScript
+{
+    public class MoveTransformAction : ActionBase
+    {
+        private enum MoveTypes
+        {
+            Teleport,
+            Lerp,
+            Slerp,
+            AnimCurve,
+        }
+
+        [Header("Action Settings")]
+        [SerializeField]
+        private Transform m_ObjectToMove = null;
+        [SerializeField]
+        private MoveTypes m_MoveType = MoveTypes.Teleport;
+        [SerializeField]
+        private Transform m_TargetPosition = null;
+        [SerializeField]
+        private float m_MoveTime = 1f;
+        [SerializeField]
+        private bool m_IsAffectedByTimescale = true;
+        [SerializeField]
+        private AnimationCurve m_AnimCurve;
+
+        private Vector3 mStartPosition = Vector3.zero;
+        private bool mIsMoving = false;
+        private float mCurrentMoveTime = 0f;
+        private float mCurrentNormalizedTime = 0f;
+
+        protected override void ActionLogic()
+        {
+            if (!mIsMoving)
+            {
+                if (m_MoveType == MoveTypes.Teleport)
+                {
+                    m_ObjectToMove.position = m_TargetPosition.position;
+                    OnActionComplete();
+                }
+                else
+                {
+                    StartCoroutine(DoMove());
+                }
+            }
+        }
+
+        private IEnumerator DoMove()
+        {
+            mIsMoving = true;
+            mCurrentMoveTime = 0f;
+            mStartPosition = m_ObjectToMove.transform.position;
+            mCurrentNormalizedTime = 0f;
+
+            while (mCurrentMoveTime < m_MoveTime)
+            {
+                mCurrentNormalizedTime = Mathf.InverseLerp(0f, m_MoveTime, mCurrentMoveTime);
+
+                if (m_MoveType == MoveTypes.Lerp)
+                {
+                    m_ObjectToMove.transform.position = Vector3.Lerp(mStartPosition, m_TargetPosition.position, mCurrentNormalizedTime);
+                }
+                else if (m_MoveType == MoveTypes.Slerp)
+                {
+                    m_ObjectToMove.transform.position = Vector3.Slerp(mStartPosition, m_TargetPosition.position, mCurrentNormalizedTime);
+                }
+                else if (m_MoveType == MoveTypes.AnimCurve)
+                {
+                    float animatedTime = m_AnimCurve.Evaluate(mCurrentNormalizedTime);
+                    m_ObjectToMove.transform.position = Vector3.Lerp(mStartPosition, m_TargetPosition.position, animatedTime);
+                }
+
+                float deltaTime = m_IsAffectedByTimescale ? Time.deltaTime : Time.unscaledDeltaTime;
+
+                mCurrentMoveTime = Mathf.Clamp(mCurrentMoveTime + deltaTime, 0f, m_MoveTime);
+
+                yield return new WaitForSeconds(deltaTime);
+            }
+
+            mIsMoving = false;
+            OnActionComplete();
+        }
+    }
+}
---
+++ SimpleScript/SimpleScript/SimpleScript/Generator/NoiseComponent.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[AddComponentMenu("Simple Script/Generators/Noise")]
+public class NoiseComponent : MonoBehaviour
+{
+    [System.Serializable]
+    private class NoiseEvent : UnityEvent<float> { };
+
+    [System.Serializable]
+    public class PerlinLayer
+    {
+        public bool LayerIsMultiplicative = false;
+        public float NoiseMagnitude = 1f;
+        public float NoiseSpeed = 1f;
+    }
+
+    public PerlinLayer[] PerlinLayers;
+
+    [SerializeField]
+    private int m_RandomizerAmount = 0;
+
+    [SerializeField]
+    private NoiseEvent m_NoiseOutput = new NoiseEvent();
+    public void FixedUpdate()
+    {
+        float timeOffset = Random.Range(-1f, 1f) * m_RandomizerAmount;
+        DoNoise(Time.realtimeSinceStartup + timeOffset);
+    }
+
+    public void DoNoise(float noiseTime)
+    {
+        float noiseValue = 0;
+        int layerCount = 0;
+
+        foreach (PerlinLayer layer in PerlinLayers)
+        {
+            float time = noiseTime * layer.NoiseSpeed;
+            float tempAmount = 0f;
+
+            tempAmount = Mathf.PerlinNoise(time, 0.0f) * layer.NoiseMagnitude;
+
+            if (layerCount == 0 || !layer.LayerIsMultiplicative)
+            {
+                noiseValue += tempAmount;
+
+            }
+            else
+            {
+                noiseValue *= tempAmount;
+            }
+
+            layerCount++;
+        }
+
+        m_NoiseOutput.Invoke(noiseValue);
+    }
+}
